ab7aa52528b0460a91a6f8901c2b41bb5ab0c061
shovity <shovity@gmail.com>
2021-05-29 20:28:02 +0700

    add tabs

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react'
+import { Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+import Chat from './components/Chat'
+import Diary from './components/Diary'
+import Profile from './components/Profile'
+import Together from './components/Together'
+
+
+const Tab = createBottomTabNavigator()
+
+const tabIcon = {
+  Together: 'heart-outline',
+  Diary: 'book-outline',
+  Chat: 'chatbox-ellipses-outline',
+  Profile: 'person-circle-outline',
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            return <Ionicons name={tabIcon[route.name]} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: '#F44336',
+          inactiveTintColor: '#9E9E9E',
+        }}
+      >
+        <Tab.Screen name='Together' component={Together} />
+        <Tab.Screen name='Diary' component={Diary} />
+        <Tab.Screen name='Chat' component={Chat} />
+        <Tab.Screen name='Profile' component={Profile} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
